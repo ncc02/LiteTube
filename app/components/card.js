2c3f1d7a0e9184f63198e23b5c81c89feaa26c19
ncc02 <cuongtk2002@gmail.com>
2024-11-22 22:10:53 +0700

    changes/card.js

--- app/components/card.js
+++ app/components/card.js
@@ -2,7 +2,7 @@
 export default function Card({title, description, img}){
     return(
 
-        <div className="card bg-[#262626]  md:w-96 h-64 shadow-xl rounded-none ">
+        <div className="card bg-[#262626]  md:w-96 h-64 shadow-xl rounded-none">
         <figure>
           <img
             src={img ? img : "https://cdn.tgdd.vn/Files/2016/12/30/931979/-windowsxpaa_800x450.jpg"}
@@ -12,7 +12,7 @@ export default function Card({title, description, img}){
           <span>{title}</span>  
 
         </div>
-        <div className="p-2 h-1/6 overflow-hidden">
+        <div className="px-2 h-1/6 overflow-hidden mb-2">
           <p className="text-xs">{description ? description.split(' ').slice(0, 15).join(' ') + '...' : ''}</p>
           </div>
       </div>
